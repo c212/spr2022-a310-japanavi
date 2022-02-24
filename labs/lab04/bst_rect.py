d27c62f291c325cd2095217174ab4958ce1516c5
japanavi <apanavicius.josh146@gmail.com>
2022-02-24 16:27:19 -0500

    Worked on Lab06

---
+++ labs/lab04/bst_rect.py
@@ -0,0 +1,100 @@
+import cProfile
+import pstats
+
+
+class BstNode:
+    def __init__(self, key):
+        self.key = key
+        self.right = None
+        self.left = None
+
+    def insert(self, key):
+        if self.key == key:
+            return
+        elif self.key < key:
+            if self.right is None:
+                self.right = BstNode(key)
+            else:
+                self.right.insert(key)
+        else:  # self.key > key
+            if self.left is None:
+                self.left = BstNode(key)
+            else:
+                self.left.insert(key)
+
+    def display(self):
+        lines, *_ = self._display_aux()
+        for line in lines:
+            print(line)
+            # print(*_)
+
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # Base case #1: No child.
+        if self.right is None and self.left is None:
+            line = "%s" % self.key
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Base case #2: Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = "%s" % self.key
+            u = len(s)
+            first_line = (x + 1) * " " + (n - x - 1) * "_" + s
+            second_line = x * " " + "|" + (n - x - 1 + u) * " "
+            shifted_lines = [line + u * " " for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Base case #3: Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = "%s" % self.key
+            u = len(s)
+            first_line = s + x * "_" + (n - x) * " "
+            second_line = (u + x) * " " + "|" + (n - x - 1) * " "
+            shifted_lines = [u * " " + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = "%s" % self.key
+        u = len(s)
+        first_line = (x + 1) * " " + (n - x - 1) * "_" + s + y * "_" + (m - y) * " "
+        second_line = (
+            x * " " + "|" + (n - x - 1 + u + y) * " " + "|" + (m - y - 1) * " "
+        )
+        if p < q:
+            left += [n * " "] * (q - p)
+        elif q < p:
+            right += [m * " "] * (p - q)
+
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * " " + b for a, b in zipped_lines]
+
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+from random import randint, seed
+
+
+def main():
+
+    with cProfile.Profile() as pr:
+        seed(41)
+        b = BstNode(50)
+        for _ in range(50):
+            b.insert(randint(0, 100))
+        b.display()
+
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.CALLS)
+    stats.print_stats()
+
+
+if __name__ == "__main__":
+    main()
+
